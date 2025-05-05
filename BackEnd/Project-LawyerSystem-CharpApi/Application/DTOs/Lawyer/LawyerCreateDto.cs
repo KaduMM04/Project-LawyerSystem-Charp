6db0207f2309c395f4bb0722c331c14173f0ef3d
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-05-05 17:38:13 -0300

    Using data Annotation

--- BackEnd/Project-LawyerSystem-CharpApi/Application/DTOs/Lawyer/LawyerCreateDto.cs
+++ BackEnd/Project-LawyerSystem-CharpApi/Application/DTOs/Lawyer/LawyerCreateDto.cs
@@ -1,20 +1,29 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Project_LawyerSystem_CharpApi.Application.DTOs.Lawyer;
+public class LawyerCreateDto
+{
+    /// <summary>
+    /// Gets or sets the OAB (Order of Attorneys of Brazil) number of the lawyer.
+    /// </summary>
+    [NotNull]
+    [Required]
+    [JsonPropertyName("OAB")]
+    [StringLength(20, ErrorMessage = "OAB must be up to 20 characters")]
+    public string OAB { get; set; } = string.Empty;
 
-    public class LawyerCreateDto
-    {
-        /// <summary>
-        /// Gets or sets the OAB (Order of Attorneys of Brazil) number of the lawyer.
-        /// </summary>
-        public string OAB { get; set; } = string.Empty;
-
-        /// <summary>
-        /// Gets or sets the area of expertise of the lawyer.
-        /// </summary>
-        public string AreaOfExpertise { get; set; } = string.Empty;
-        
-    }
+    /// <summary>
+    /// Gets or sets the area of expertise of the lawyer.
+    /// </summary>
+    [NotNull]
+    [JsonPropertyName("AreaOfExpertise")]
+    [Required(ErrorMessage = "Area Of Expertise is required")]
+    [StringLength(20, ErrorMessage = "Area Of Expertise must be up to 11")]
+    public string AreaOfExpertise { get; set; } = string.Empty;
+}
