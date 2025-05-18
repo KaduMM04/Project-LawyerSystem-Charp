fa80d779eb7355704ba80b34d3799321ea74106e
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-05-18 16:08:49 -0300

    Created the InitialPage

---
+++ LawyerSystem.Client/src/Pages/InitialPage/InitialPage.jsx
@@ -0,0 +1,31 @@
+import Button from "../../Components/Button";
+import Container from "../../Components/Container";
+import { useNavigate } from 'react-router-dom';
+
+function InitialPage() {
+
+    const navigate = useNavigate();
+
+    return (
+        <>
+            <Container>
+
+                <div>
+                    <h1>Welcome to the Lawyer System</h1>
+                    <p>This is the initial page of the application.</p>
+                </div>
+
+                <Button
+                    text="Register Lawyer"
+                    type="button"
+                    Class="PatterButton"
+                    onClick={() => navigate('/registerlawyer')}
+                />
+
+            </Container>
+
+        </>
+    );
+}
+
+export default InitialPage;
