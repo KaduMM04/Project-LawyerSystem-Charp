0a4eded1fd4af1da88169a1b27a7bb5c825de4e8
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-05-22 22:13:36 -0300

    Made First Version of the Initial Page

--- LawyerSystem.Client/src/Pages/InitialPage/InitialPage.jsx
+++ LawyerSystem.Client/src/Pages/InitialPage/InitialPage.jsx
@@ -9,39 +9,44 @@ function InitialPage() {
     return (
         <>
             
-                
-                <Button
-                    text="Processos Ativos"
-                    type="button"
-                    Class="initial-button"
-                    onClick={() => navigate('/registerlawyer')}
-                />
-                <Button
-                    text="Agenda"
-                    type="button"
-                    Class="initial-button"
-                    onClick={() => navigate('/registerclient')}
-                />
+            <div id="initial">
+
+                <div id="buttons-group">
+                    <Button
+                        text="Processos Ativos"
+                        type="button"
+                        Class="initial-button"
+                        onClick={() => navigate('/registerlawyer')}
+                    />
+                    <Button
+                        text="Agenda"
+                        type="button"
+                        Class="initial-button"
+                        onClick={() => navigate('/registerclient')}
+                    />
+                </div>
 
                 <div id="next-audience">
                     <h2>Proximas Audiencias</h2>
-                    <hr></hr>
+                    <hr />
                     <div id="audience-info">
                         <h5>11/08/2025</h5>
                         <h5>Tony Vs Steven</h5>
-                    
-                   
                         <h5>30/02/2029</h5>
                         <h5>Caso Souza</h5>
-                    
-                    
                         <h5>25/12/1784</h5>
-                        <h5>sei la </h5>
+                        <h5>sei lá</h5>
                     </div>
-                    
                 </div>
-          
-        
+
+                <div id="recent-process">
+                    <h2>Processos Recentes:</h2>
+                    <div id="recent-cases">
+                        
+                    </div>
+                </div>
+
+            </div>
         </>
     );
 }
